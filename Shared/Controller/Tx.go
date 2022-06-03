e5d807800e86dbe9ef65972fb466af33ea2e869a
ericpatricksantos <ericpatrick.santos@ufu.br>
2022-06-03 20:07:35 -0300

    Organizando funções

--- Shared/Controller/Tx.go
+++ Shared/Controller/Tx.go
@@ -1,11 +1,12 @@
 package Controller
 
 import (
-	"fmt"
 	"Tcc/Shared/API"
 	"Tcc/Shared/Function"
 	"Tcc/Shared/Model"
+	"fmt"
 	"strconv"
+	"time"
 )
 
 // SaveTxs Salva todas as transações de um Block no MongoDB
@@ -95,4 +96,271 @@ func DeleteTxMongo(hash string, ConnectionMongoDB string, DataBaseMongo string, 
 
 func GetTxMongoDB(ConnectionMongoDB string, DataBaseMongo string, CollectionRecuperaDados string) Model.Transaction {
 	return Function.GetTxMongoDB(ConnectionMongoDB, DataBaseMongo, CollectionRecuperaDados)
+}
+
+func BuscaEnderecosDistancia1() {
+	fmt.Println("-- MultiAddr ---")
+	BuscaEnderecosDistancia1_MultiAddr()
+	fmt.Println("-- RawAddr ---")
+	BuscaEnderecosDistancia1_RawAddr()
+}
+
+func BuscaEnderecosDistancia1_RawAddr() {
+	encerraExecucao := false
+	urlAPI := "https://blockchain.info"
+	ConnectionMongoDB := "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb"
+	Database := "Endereco"
+	infoDistancia1 := Function.GetInfoMongoDB(ConnectionMongoDB,
+		Database, "Distancia1Informacoes")
+	enderecos := infoDistancia1.InfoEnderecos.Enderecos
+	tamanhoEnderecos := len(enderecos)
+	indiceInicial := Function.BuscaIndice("indice_lista_enderecos.txt")
+	tempo := 10
+
+	for indice := indiceInicial; indice < tamanhoEnderecos; indice++ {
+
+		if encerraExecucao {
+			break
+		}
+
+		if len(enderecos[indice]) < 1 {
+			encerraExecucao = true
+			fmt.Println(" Endereco vazio ")
+			break
+		}
+
+		endereco := enderecos[indice]
+		fmt.Println("Indice: ", indice)
+		fmt.Println("Buscando o endereco: ", endereco)
+		resposta := Function.GetEndereco(endereco, urlAPI, "/rawaddr/", 2000, 0)
+
+		if len(resposta.Address) < 1 || len(resposta.Txs) < 1 {
+			fmt.Println(" Erro no retorno das informações do endereço: ", endereco)
+			encerraExecucao = true
+			break
+		}
+		tamanhoTxsRecebidas := 0
+		N_tx := resposta.N_tx
+		listaEnderecos := []Model.Endereco{}
+		listaEnderecos = append(listaEnderecos, resposta)
+		fmt.Println("Tamanho das Transações: ", N_tx)
+		for {
+			if encerraExecucao {
+				break
+			}
+			tamanhoTxsRecebidas = tamanhoTxsRecebidas + len(resposta.Txs)
+			contSalve := 0
+			if tamanhoTxsRecebidas >= N_tx {
+				tamanholistaEnderecos := len(listaEnderecos)
+				fmt.Println("Salvando o Endereco: ", endereco, " no MongoDB ")
+				fmt.Println("Tamanho da lista que contem todas as transações do ", endereco, " é ", tamanholistaEnderecos)
+				for _, list_end := range listaEnderecos {
+					confirmSalve := Function.SaveAddrSimplificado(list_end, ConnectionMongoDB, Database, "Distancia1")
+
+					if confirmSalve {
+						contSalve = contSalve + 1
+					}
+					if contSalve == tamanholistaEnderecos {
+						fmt.Println("Endereco Salvo ", endereco)
+						Function.IncrementaIndice(indice, "indice_lista_enderecos.txt")
+					} else if !confirmSalve {
+						fmt.Println("Falha ao salva endereco ", endereco)
+						encerraExecucao = true
+						break
+					}
+				}
+				if contSalve == tamanholistaEnderecos {
+					break
+				}
+			} else {
+				time.Sleep(time.Second * time.Duration(tempo))
+				resposta = Function.GetEndereco(endereco, urlAPI, "/rawaddr/", 2000, tamanhoTxsRecebidas)
+				if len(resposta.Address) < 1 || len(resposta.Txs) < 1 {
+					fmt.Println(" Nao buscou os restantes da transações do endereço: ", endereco)
+					fmt.Println("Limit: ", 2000)
+					fmt.Println("Offset: ", tamanhoTxsRecebidas)
+					encerraExecucao = true
+					break
+				}
+				listaEnderecos = append(listaEnderecos, resposta)
+			}
+
+		}
+		time.Sleep(time.Second * time.Duration(tempo))
+	}
+
+}
+
+func BuscaEnderecosDistancia1_MultiAddr() {
+	encerraExecucao := false
+	urlAPI := "https://blockchain.info"
+	ConnectionMongoDB := "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb"
+	Database := "Endereco"
+	infoDistancia1 := Function.GetInfoMongoDB(ConnectionMongoDB,
+		Database, "Distancia1Informacoes")
+	enderecos := infoDistancia1.InfoEnderecos.Enderecos
+	tamanhoEnderecos := len(enderecos)
+	indiceInicial := Function.BuscaIndice("indice_lista_enderecos.txt")
+	tempo := 10
+
+	for indice := indiceInicial; indice < tamanhoEnderecos; indice++ {
+
+		if encerraExecucao {
+			break
+		}
+
+		if len(enderecos[indice]) < 1 {
+			encerraExecucao = true
+			fmt.Println(" Endereco vazio ")
+			break
+		}
+
+		endereco := enderecos[indice]
+		fmt.Println("Indice: ", indice)
+		fmt.Println("Buscando o endereco: ", endereco)
+		respostaMultiAddr := Function.GetMultiAddr([]string{endereco}, urlAPI, "/multiaddr?active=", 2000, 0)
+		resposta := Function.ConverteMultiAddrParaAddr(respostaMultiAddr)
+		if len(resposta.Address) < 1 || len(resposta.Txs) < 1 {
+			fmt.Println(" Erro no retorno das informações do endereço: ", endereco)
+			encerraExecucao = true
+			break
+		}
+		tamanhoTxsRecebidas := 0
+		N_tx := resposta.N_tx
+		listaEnderecos := []Model.Endereco{}
+		listaEnderecos = append(listaEnderecos, resposta)
+		fmt.Println("Tamanho das Transações: ", N_tx)
+		for {
+			if encerraExecucao {
+				break
+			}
+			tamanhoTxsRecebidas = tamanhoTxsRecebidas + len(resposta.Txs)
+			contSalve := 0
+			if tamanhoTxsRecebidas >= N_tx {
+				tamanholistaEnderecos := len(listaEnderecos)
+				fmt.Println("Salvando o Endereco: ", endereco, " no MongoDB ")
+				fmt.Println("Tamanho da lista que contem todas as transações do ", endereco, " é ", tamanholistaEnderecos)
+				for _, list_end := range listaEnderecos {
+					confirmSalve := Function.SaveAddrSimplificado(list_end, ConnectionMongoDB, Database, "Distancia1")
+
+					if confirmSalve {
+						contSalve = contSalve + 1
+					}
+					if contSalve == tamanholistaEnderecos {
+						Function.IncrementaIndice(indice, "indice_lista_enderecos.txt")
+						fmt.Println("Endereco Salvo ", endereco)
+					} else if !confirmSalve {
+						fmt.Println("Falha ao salva endereco ", endereco)
+						encerraExecucao = true
+						break
+					}
+				}
+				if contSalve == tamanholistaEnderecos {
+					break
+				}
+			} else {
+				time.Sleep(time.Second * time.Duration(tempo))
+				respostaMultiAddr = Function.GetMultiAddr([]string{endereco}, urlAPI, "/multiaddr?active=", 2000, tamanhoTxsRecebidas)
+				resposta = Function.ConverteMultiAddrParaAddr(respostaMultiAddr)
+				if len(resposta.Address) < 1 || len(resposta.Txs) < 1 {
+					fmt.Println(" Nao buscou os restantes da transações do endereço: ", endereco)
+					fmt.Println("Limit: ", 100)
+					fmt.Println("Offset: ", tamanhoTxsRecebidas)
+					encerraExecucao = true
+					break
+				}
+				listaEnderecos = append(listaEnderecos, resposta)
+			}
+
+		}
+		time.Sleep(time.Second * time.Duration(tempo))
+	}
+
+}
+
+func BuscaEnderecosDistancia1_Dividindo_Transacoes() {
+	encerraExecucao := false
+	urlAPI := "https://blockchain.info"
+	ConnectionMongoDB := "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb"
+	Database := "Endereco"
+	infoDistancia1 := Function.GetInfoMongoDB(ConnectionMongoDB,
+		Database, "Distancia1Informacoes")
+	enderecos := infoDistancia1.InfoEnderecos.Enderecos
+	tamanhoEnderecos := len(enderecos)
+	indiceInicial := Function.BuscaIndice("indice_lista_enderecos.txt")
+	tempo := 5
+
+	for indice := indiceInicial; indice < tamanhoEnderecos; indice++ {
+
+		if encerraExecucao {
+			break
+		}
+
+		if len(enderecos[indice]) < 1 {
+			fmt.Println(" Endereco vazio ")
+			break
+		}
+
+		endereco := enderecos[indice]
+		fmt.Println("Indice: ", indice)
+		fmt.Println("Buscando o endereco: ", endereco)
+		resposta := Function.GetEndereco(endereco, urlAPI, "/rawaddr/", 1000, 0)
+
+		if len(resposta.Address) < 1 || len(resposta.Txs) < 1 {
+			fmt.Println(" Erro no retorno das informações do endereço: ", endereco)
+			break
+		}
+		tamanhoTxsRecebidas := 0
+		N_tx := resposta.N_tx
+		respostaCompleta := resposta
+
+		for {
+			if encerraExecucao {
+				break
+			}
+			tamanhoTxsRecebidas = tamanhoTxsRecebidas + len(respostaCompleta.Txs)
+			contSalve := 0
+			if tamanhoTxsRecebidas == N_tx {
+				listaEnderecos := Function.DividiTransacoesDosEndereco(respostaCompleta, 1000.0)
+				if listaEnderecos == nil || len(listaEnderecos) == 0 {
+					encerraExecucao = true
+					break
+				}
+				tamanholistaEnderecos := len(listaEnderecos)
+				fmt.Println("Salvando o Endereco: ", endereco, " no MongoDB ")
+				fmt.Println(" Tamanho da lista total de transações: ", N_tx)
+				fmt.Println("Tamanho da lista que contem todas as transações do ", endereco, " e ", tamanholistaEnderecos)
+				for _, list_end := range listaEnderecos {
+					confirmSalve := Function.SaveAddrSimplificado(list_end, ConnectionMongoDB, Database, "Distancia1")
+
+					if confirmSalve {
+						contSalve = contSalve + 1
+					}
+					if contSalve == tamanholistaEnderecos {
+						fmt.Println("Endereco Salvo ", endereco)
+						Function.IncrementaIndice(indice, "indice_lista_enderecos.txt")
+					} else if !confirmSalve {
+						fmt.Println("Falha ao salva endereco ", endereco)
+						encerraExecucao = true
+						break
+					}
+				}
+				if contSalve == tamanholistaEnderecos {
+					break
+				}
+			} else {
+				time.Sleep(time.Second * time.Duration(tempo))
+				resposta = Function.GetEndereco(endereco, urlAPI, "/rawaddr/", 1000, tamanhoTxsRecebidas)
+				if len(resposta.Address) < 1 || len(resposta.Txs) < 1 {
+					fmt.Println(" Nao buscou os restantes da transações")
+					encerraExecucao = true
+					break
+				}
+				respostaCompleta.Txs = append(respostaCompleta.Txs, resposta.Txs...)
+			}
+
+		}
+		time.Sleep(time.Second * time.Duration(tempo))
+	}
+
 }
---
+++ Validations/validations.go
@@ -0,0 +1,236 @@
+package main
+
+import (
+	"Tcc/Shared/Function"
+	"Tcc/Shared/Model"
+	"fmt"
+)
+
+var limit int64 = 100
+var ConnectionMongo string = "mongodb://127.0.0.1:27017/"
+var DB_Endereco string = "Endereco"
+var DB_Cluster string = "Cluster"
+var Collection_Cluster_Map string = "Clusters"
+
+func main() {
+	// validei o backup 03/06/2022
+	VerificaEnderecosTotaisClusters()
+}
+
+func TransferindoProd() {
+	f := Function.TransfereClustersD1_D2(ConnectionMongo, "Cluster", "Cluster",
+		"Identificadores_1", "Distancia1", "Identificadores_2", "Distancia2",
+		"Cluster", "Identificadores", "Clusters")
+
+	fmt.Println(f)
+}
+
+func TransferindoDev() {
+	f := Function.TransfereClustersD1_D2(ConnectionMongo, "Cluster", "Cluster",
+		"Identificadores_1", "Distancia1", "Identificadores_2", "Distancia2",
+		"teste", "Identificadores", "Clusters")
+
+	fmt.Println(f)
+}
+
+func ContagemEnderecosTotais() {
+	r := Function.ContagemEnderecosTotais(ConnectionMongo, DB_Cluster, "Distancia1", "Distancia2")
+
+	// Total 1530531 endereços distancia 1 e 2
+	fmt.Println(len(r))
+}
+
+func VerificaEnderecosTotaisClusters() {
+	f := Function.ContagemEnderecosClusters(ConnectionMongo, "Backup", "Clusters1")
+	// Total 1530531 endereços
+	fmt.Println(len(f))
+}
+
+func VerificaQtdTransacoesTotais() {
+	v := Function.UnionHashTransacaoD1_D2(ConnectionMongo, DB_Endereco, "Farao", "Distancia1")
+	// Distancia 1 - 278 transações/clusters
+	// Distancia 2 - 633314 transações/clusters
+
+	// Total 633592 transações/clusters
+	fmt.Println(len(v))
+}
+
+func VerificaEnderecosTotais() {
+	v := Function.UnionEnderecosD1_D2(ConnectionMongo, DB_Endereco, "Farao", "Distancia1")
+	// Distancia 1 -  6528 endereços
+	// Distancia 2 - 1524003 endereços
+
+	// Total 1530531 endereços
+	fmt.Println(len(v))
+}
+
+func VerificaQtdEnderecosDistancia1() {
+	colDist1 := "Distancia1"
+	clusters := Function.GetAllClustersLimit(100000, ConnectionMongo, DB_Cluster, colDist1)
+	lista := []string{}
+
+	for _, cluster := range clusters {
+		for _, endereco := range cluster.Clusters {
+			check := Function.Contains(lista, endereco)
+			if check {
+				continue
+			} else {
+				lista = append(lista, endereco)
+			}
+		}
+	}
+	if len(lista) == 6528 {
+		fmt.Println(" Correto")
+	}
+
+}
+
+// em uso
+func VerificaCreateClustersDistancia1_Map() {
+	colDist1 := "Distancia1"
+	clusters := Function.GetAllMapClustersLimit(100000, ConnectionMongo, DB_Cluster, colDist1)
+	mapas := map[string]string{}
+
+	for _, cluster := range clusters {
+		for endereco, _ := range cluster.Clusters {
+			_, ok := mapas[endereco]
+			if ok {
+				continue
+			} else {
+				mapas[endereco] = endereco
+			}
+		}
+	}
+
+	if len(mapas) == 6528 {
+		fmt.Println(" Correto")
+	}
+}
+
+func TesteDistancia() {
+	ConnectionMongoDB := "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb"
+	Database := "Endereco"
+	Collection := "Distancia1"
+
+	valores := Function.GetAllAddrLimit(100000000, ConnectionMongoDB, Database, Collection)
+	dic := map[string]string{}
+
+	for _, item := range valores {
+		_, ok := dic[item.Address]
+		if ok {
+
+		} else {
+			dic[item.Address] = item.Address
+		}
+	}
+
+	fmt.Println(len(dic))
+}
+
+func TesteDistancia1Informacoes() {
+	inputs := []string{}
+	txs := []string{}
+	enderecos := Function.GetAllAddr("mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb",
+		"Endereco", "Farao")
+
+	for _, endereco := range enderecos {
+		for _, tx := range endereco.Txs {
+			txs = append(txs, tx.Hash)
+			for _, input := range tx.Inputs {
+				inputs = append(inputs, input.Prev_Out.Addr)
+			}
+		}
+	}
+
+	inputUnicos, _ := Function.RemoveDuplicados(inputs)
+	txsUnicos, _ := Function.RemoveDuplicados(txs)
+
+	info := Model.Informacoes{
+		InfoEnderecos: Model.InfoEnderecos{
+			Enderecos:    inputUnicos,
+			QtdEnderecos: len(inputUnicos),
+		},
+		InfoTransacoes: Model.InfoTransacoes{
+			Transacoes:    txsUnicos,
+			QtdTransacoes: len(txsUnicos),
+		},
+	}
+
+	Function.SaveInfo(info, "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb",
+		"Endereco", "Distancia1Informacoes")
+}
+
+func TesteEndereco_indice_2053() {
+	ConnectionMongoDB := "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb"
+	Database := "Endereco"
+	Collection := "Endereco_2053"
+	enderecos := Function.GetAllAddrLimit(1000000, ConnectionMongoDB,
+		Database, Collection)
+	dic := map[string]string{}
+	//hashes := []string{}
+	for _, addr := range enderecos {
+		for _, tx := range addr.Txs {
+			//hashes = append(hashes, tx.Hash)
+			_, ok := dic[tx.Hash]
+			if ok {
+				continue
+			} else {
+				dic[tx.Hash] = tx.Hash
+			}
+		}
+	}
+	//x,_ := Function.RemoveDuplicados(hashes)
+
+	//mt.Println(len(hashes))
+	//fmt.Println(len(x))
+	fmt.Println(len(dic))
+}
+
+func QuantidadeTransacoesDistancia1() {
+	ConnectionMongoDB := "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb"
+	Database := "teste"
+	Collection := "transacoes"
+	enderecos := Function.GetAllAddrLimit(1000000, ConnectionMongoDB,
+		Database, Collection)
+	dic := map[string]string{}
+	hashes := []string{}
+	contador := 0
+	for _, addr := range enderecos {
+		x := len(addr.Txs)
+		contador = contador + x
+		for _, tx := range addr.Txs {
+			hashes = append(hashes, tx.Hash)
+			_, ok := dic[tx.Hash]
+			if ok {
+				continue
+			} else {
+				dic[tx.Hash] = tx.Hash
+			}
+		}
+	}
+	fmt.Println("Qtd: ", contador)
+	fmt.Println(len(hashes))
+	fmt.Println(len(dic))
+}
+
+func ContEnderecosCluster() {
+	clusters := Function.GetAllMapClustersLimit(limit, ConnectionMongo, DB_Cluster, Collection_Cluster_Map)
+	v := map[string]string{}
+
+	for _, item := range clusters {
+		for ch, _ := range item.Clusters {
+			_, ok := v[ch]
+			if !ok {
+				v[ch] = ""
+			}
+		}
+	}
+
+	fmt.Println(len(v))
+}
+
+func VerificaEnderecosTotaisClusters_V2() {
+	f := Function.ContagemEnderecosClusters(ConnectionMongo, "Cluster", "Clusters")
+	// Total 1530531 endereços
+	fmt.Println(len(f))
+}
